5c7ff22833ef472d0526613dc4a09c1fb91b2909
Firassaad <36532618+Firassaad@users.noreply.github.com>
2023-04-04 03:06:57 +0200

    Update ApplicationTest.java

--- Application/ApplicationTest.java
+++ Application/ApplicationTest.java
@@ -8,19 +8,26 @@ import Application.Entities.Spectateur;
 
 @Test
 public class ApplicationTest {
-        Spectateur spectateur = new Spectateur("Spectateur 1");
+        Spectateur spectateur = new Spectateur("Spectateur");
         Dresseur dresseur1 = new Dresseur("Dresseur 1");
         Dresseur dresseur2 = new Dresseur("Dresseur 2");
-        Singe singe1 = new Singe("Singe 1", new Tour[] { new TourAcrobatie("Tour acrobatie 1"), new TourMusique("Tour musique 1") });
-        Singe singe2 = new Singe("Singe 2", new Tour[] { new TourAcrobatie("Tour acrobatie 2"), new TourMusique("Tour musique 2") });
+        Singe singe1 = new Singe("Singe 1", new Tour[] { new Tour("Acrobatie : marcher sur les mains",Type.ACROBATIE), 
+                                                         new Tour("Musique : jouer de la guitare", Type.MUSIQUE) });
+        Singe singe2 = new Singe("Singe 2", new Tour[] { new Tour("Acrobatie : rolade avant",Type.ACROBATIE), 
+                                                         new Tour("Musique : mimer des chanteurs sur l'écran", Type.MUSIQUE) });
+ 
+ 
+     Dresseur dresseur1 = new Dresseur(singe1);
+     Dresseur dresseur2 = new Dresseur(singe2);
 
-        dresseur1.faireExecuterTousLesTours(singe1, spectateur);
-        dresseur2.faireExecuterTousLesTours(singe2, spectateur);
+    
+        dresseur1.demanderTours(spectateur);
+        dresseur2.demanderTours(spectateur);
 
-        assertEquals("Spectateur 1 applaudit pendant le tour d'acrobatie 'Tour acrobatie 1' du singe 'Singe 1'\n" +
-                     "Spectateur 1 siffle pendant le tour de musique 'Tour musique 1' du singe 'Singe 1'\n" +
-                     "Spectateur 1 applaudit pendant le tour d'acrobatie 'Tour acrobatie 2' du singe 'Singe 2'\n" +
-                     "Spectateur 1 siffle pendant le tour de musique 'Tour musique 2' du singe 'Singe 2'\n", 
+        assertEquals("Spectateur applaudit pendant le tour d'acrobatie 'Acrobatie : marcher sur les mains' du singe 1'\n" +
+                     "Spectateur siffle pendant le tour de musique 'Musique : jouer de la guitare' du singe 1'\n" +
+                     "Spectateur applaudit pendant le tour d'acrobatie 'Acrobatie : marcher sur les mains' du singe 2'\n" +
+                     "Spectateur siffle pendant le tour de musique 'Musique : mimer des chanteurs sur l'écran' du singe 'Singe 2'\n", 
                      spectateur.getReaction());
-    }
 }
+
